7aab412ba2313aa0cb992050a6af91c033178418
Jab11n <jabinstech.net@gmail.com>
2025-07-19 23:51:46 -0400

    general navbar content, small fixes

--- static/global/elements/navbar.js
+++ static/global/elements/navbar.js
@@ -3,5 +3,18 @@ document.addEventListener("DOMContentLoaded", async () => {
         await fetch("/static/global/elements/navbar.html")
     ).text();
     if (localStorage.getItem("Username")) {
+        const un = localStorage.getItem("Username");
+        document.getElementById("nav-login").style.display = "none";
+        document.getElementById(
+            "nav-user"
+        ).src = `https://api.wasteof.money/users/${un}/picture`;
+        document.getElementById("nav-user").style.display = "block";
+        document.getElementById("nav-compose").style.display = "flex";
+    } else {
+        document.getElementById("nav-login").style.display = "block";
+    }
+    if (window.location.pathname == "/") {
+        document.getElementById("nav-go-home").className =
+            document.getElementById("nav-go-home").className + " nav-loc-cur";
     }
 });
