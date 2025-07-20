d9118724bdbc26c8769bb8836d37b358cae56ea2
Jab11n <jabinstech.net@gmail.com>
2025-07-20 00:10:08 -0400

    some things idrk

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, request, jsonify, send_from_directory, render_template
-import requests
-import os
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home-loggedout.html')
 
 @app.route('/users/<username>')
 def user_profile(username):
