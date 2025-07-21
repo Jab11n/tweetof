16d68a2f299116cf60e3a69b44714adec8f8f580
Jab11n <jabinstech.net@gmail.com>
2025-07-21 10:20:31 -0400

    speedier loading 🔥

--- static/user.js
+++ static/user.js
@@ -67,28 +67,6 @@ document.addEventListener("DOMContentLoaded", async () => {
             }
         }
 
-        async function fetchUserPosts(username) {
-            const response = await fetch(
-                `https://api.wasteof.money/users/${username}/posts`
-            );
-            if (!response.ok) {
-                console.error(
-                    "Error fetching user posts:",
-                    response.statusText
-                );
-                alert(
-                    "An error occurred while fetching posts. " +
-                        response.statusText +
-                        " (" +
-                        response.status +
-                        ")"
-                );
-                return null;
-            } else {
-                return await response.json();
-            }
-        }
-
         const userData = await fetchUserData(username);
 
         if (userData) {
@@ -161,164 +139,158 @@ document.addEventListener("DOMContentLoaded", async () => {
             }
         }
 
+        async function fetchUserPosts() {
+            const response = await fetch(
+                `https://api.wasteof.money/users/${username}/posts`
+            );
+            return await response.json();
+        }
+
         const userPosts = await fetchUserPosts(username);
 
-        if (userPosts) {
-            for (let i = 0; i < userPosts.posts.length; i++) {
-                let post = userPosts.posts[i];
-                if (post) {
-                    let postElement = document.createElement("div");
-                    postElement.className = "post-compact";
+        async function loadPost(post) {
+            if (!post) {
+                return;
+            }
+            let postElement = document.createElement("div");
+            postElement.className = "post-compact";
 
-                    if (post.repost && post.content == "") {
-                        let retweetElement = document.createElement("div");
-                        retweetElement.className = "retweet";
-                        retweetElement.innerHTML = `<i class="fa-solid fa-repeat"></i>&ensp;${post.poster.name} reposted`;
-                        postElement.appendChild(retweetElement);
-                    }
-                    let postInnerElement = document.createElement("div");
-                    postInnerElement.className = "post-inner1";
+            if (post.repost && post.content == "") {
+                let retweetElement = document.createElement("div");
+                retweetElement.className = "retweet";
+                retweetElement.innerHTML = `<i class="fa-solid fa-repeat"></i>&ensp;${post.poster.name} reposted`;
+                postElement.appendChild(retweetElement);
+            }
+            let postInnerElement = document.createElement("div");
+            postInnerElement.className = "post-inner1";
 
-                    let postPfp = document.createElement("img");
-                    postPfp.className = "post-pfp";
-                    postPfp.src =
-                        "https://api.wasteof.money/users/" +
-                        post.poster.name +
-                        "/picture";
-                    postInnerElement.appendChild(postPfp);
+            let postPfp = document.createElement("img");
+            postPfp.className = "post-pfp";
+            postPfp.src =
+                "https://api.wasteof.money/users/" +
+                post.poster.name +
+                "/picture";
+            postInnerElement.appendChild(postPfp);
 
-                    let postContent = document.createElement("div");
-                    postContent.className = "post-content";
+            let postContent = document.createElement("div");
+            postContent.className = "post-content";
 
-                    let posterInfo = document.createElement("div");
-                    posterInfo.className = "poster-info";
-                    posterInfo.innerHTML = `
+            let posterInfo = document.createElement("div");
+            posterInfo.className = "poster-info";
+            posterInfo.innerHTML = `
                     <a class="poster-name">@${post.poster.name}</a>
                     <p class="post-time">${new Date(
                         post.time
                     ).toLocaleString()}</p>
                 `;
-                    postContent.appendChild(posterInfo);
+            postContent.appendChild(posterInfo);
 
-                    let postText = document.createElement("div");
-                    postText.className = "post-text";
-                    postText.innerHTML = post.content;
-                    postContent.appendChild(postText);
+            let postText = document.createElement("div");
+            postText.className = "post-text";
+            postText.innerHTML = post.content;
+            postContent.appendChild(postText);
 
-                    if (post.repost) {
-                        let res = await fetch(
-                            `https://api.wasteof.money/posts/${post.repost._id}`,
-                            {
-                                method: "GET",
-                            }
-                        );
-                        let repostInfo = await res.json();
-                        if (res.ok) {
-                            let retweetContent = document.createElement("div");
-                            retweetContent.className = "retweetcontent";
-                            let retweetContentContainer =
-                                document.createElement("div");
-                            retweetContentContainer.className = "post-inner1";
-                            let retweetAuthorPicture =
-                                document.createElement("img");
-                            retweetAuthorPicture.className = "post-pfp";
-                            retweetAuthorPicture.src =
-                                "https://api.wasteof.money/users/" +
-                                repostInfo.poster.name +
-                                "/picture";
-                            let retweetContentContent =
-                                document.createElement("div");
-                            retweetContentContent.className = "post-content";
-                            let retweetPosterInfo =
-                                document.createElement("div");
-                            retweetPosterInfo.className = "poster-info";
-                            let retweetAuthorName = document.createElement("a");
-                            retweetAuthorName.className = "poster-name";
-                            retweetAuthorName.href =
-                                "/users/" + repostInfo.poster.name;
-                            retweetAuthorName.innerText =
-                                repostInfo.poster.name;
-                            let retweetPostTime = document.createElement("p");
-                            retweetPostTime.className = "post-time";
-                            retweetPostTime.innerText = new Date(
-                                repostInfo.time
-                            ).toLocaleString();
-                            let repostText = document.createElement("div");
-                            repostText.className = "post-text";
-                            repostText.innerHTML = repostInfo.content;
-
-                            retweetContent.appendChild(retweetContentContainer);
-                            retweetContentContainer.appendChild(
-                                retweetAuthorPicture
-                            );
-                            retweetContentContainer.appendChild(
-                                retweetContentContent
-                            );
-                            retweetContentContent.appendChild(
-                                retweetPosterInfo
-                            );
-                            retweetPosterInfo.appendChild(retweetAuthorName);
-                            retweetPosterInfo.appendChild(retweetPostTime);
-                            retweetContentContent.appendChild(repostText);
-
-                            postContent.appendChild(retweetContent);
-
-                            retweetContent.addEventListener("click", (e) => {
-                                e.stopPropagation();
-                                window.location.href = `/posts/${repostInfo._id}`;
-                            });
-                        }
-                    } else if (post.repost === null) {
-                        let retweetContent = document.createElement("div");
-                        retweetContent.className = "retweetcontent";
-                        retweetContent.innerText =
-                            "The post that was reposted has been deleted.";
-                        postContent.appendChild(retweetContent);
+            if (post.repost) {
+                let res = await fetch(
+                    `https://api.wasteof.money/posts/${post.repost._id}`,
+                    {
+                        method: "GET",
                     }
+                );
+                let repostInfo = await res.json();
+                if (res.ok) {
+                    let retweetContent = document.createElement("div");
+                    retweetContent.className = "retweetcontent";
+                    let retweetContentContainer = document.createElement("div");
+                    retweetContentContainer.className = "post-inner1";
+                    let retweetAuthorPicture = document.createElement("img");
+                    retweetAuthorPicture.className = "post-pfp";
+                    retweetAuthorPicture.src =
+                        "https://api.wasteof.money/users/" +
+                        repostInfo.poster.name +
+                        "/picture";
+                    let retweetContentContent = document.createElement("div");
+                    retweetContentContent.className = "post-content";
+                    let retweetPosterInfo = document.createElement("div");
+                    retweetPosterInfo.className = "poster-info";
+                    let retweetAuthorName = document.createElement("a");
+                    retweetAuthorName.className = "poster-name";
+                    retweetAuthorName.href = "/users/" + repostInfo.poster.name;
+                    retweetAuthorName.innerText = repostInfo.poster.name;
+                    let retweetPostTime = document.createElement("p");
+                    retweetPostTime.className = "post-time";
+                    retweetPostTime.innerText = new Date(
+                        repostInfo.time
+                    ).toLocaleString();
+                    let repostText = document.createElement("div");
+                    repostText.className = "post-text";
+                    repostText.innerHTML = repostInfo.content;
 
-                    postInnerElement.appendChild(postContent);
-                    postElement.appendChild(postInnerElement);
+                    retweetContent.appendChild(retweetContentContainer);
+                    retweetContentContainer.appendChild(retweetAuthorPicture);
+                    retweetContentContainer.appendChild(retweetContentContent);
+                    retweetContentContent.appendChild(retweetPosterInfo);
+                    retweetPosterInfo.appendChild(retweetAuthorName);
+                    retweetPosterInfo.appendChild(retweetPostTime);
+                    retweetContentContent.appendChild(repostText);
 
-                    let postActions = document.createElement("div");
-                    postActions.className = "post-actions";
-                    let postActionsInsert;
-                    if (localStorage.getItem("Token")) {
-                        let res = await fetch(
-                            `https://api.wasteof.money/posts/${
-                                post._id
-                            }/loves/${localStorage.getItem("Username")}`,
-                            {
-                                method: "GET",
-                                headers: {
-                                    Authorization:
-                                        localStorage.getItem("Token"),
-                                },
-                            }
-                        );
-                        let resData = await res.json();
-                        if (res.ok && resData.loved === "true") {
-                            postActionsInsert = `
+                    postContent.appendChild(retweetContent);
+
+                    retweetContent.addEventListener("click", (e) => {
+                        e.stopPropagation();
+                        window.location.href = `/posts/${repostInfo._id}`;
+                    });
+                }
+            } else if (post.repost === null) {
+                let retweetContent = document.createElement("div");
+                retweetContent.className = "retweetcontent";
+                retweetContent.innerText =
+                    "The post that was reposted has been deleted.";
+                postContent.appendChild(retweetContent);
+            }
+
+            postInnerElement.appendChild(postContent);
+            postElement.appendChild(postInnerElement);
+
+            let postActions = document.createElement("div");
+            postActions.className = "post-actions";
+            let postActionsInsert;
+            if (localStorage.getItem("Token")) {
+                let res = await fetch(
+                    `https://api.wasteof.money/posts/${
+                        post._id
+                    }/loves/${localStorage.getItem("Username")}`,
+                    {
+                        method: "GET",
+                        headers: {
+                            Authorization: localStorage.getItem("Token"),
+                        },
+                    }
+                );
+                let resData = await res.json();
+                if (res.ok && resData.loved === "true") {
+                    postActionsInsert = `
                                 <a class="post-action action-like action-like-true" href="#">
                                 <i class="fa-solid fa-star"></i>
                                 <span class="like-count">${post.loves}</span>
                                 </a>
                             `;
-                        } else {
-                            postActionsInsert = `
+                } else {
+                    postActionsInsert = `
                                 <a class="post-action action-like" href="#">
                                 <i class="fa-solid fa-star"></i>
                                 <span class="like-count">${post.loves}</span>
                                 </a>
                             `;
-                        }
-                    } else {
-                        postActionsInsert = `
+                }
+            } else {
+                postActionsInsert = `
                                 <a class="post-action action-like" href="#">
                         <i class="fa-solid fa-star"></i>
                         <span class="like-count">${post.loves}</span>
                     </a>`;
-                    }
-                    postActions.innerHTML = `
+            }
+            postActions.innerHTML = `
                     <a class="post-action action-reply" href="#">
                         <i class="fa-solid fa-reply"></i>
                         <span class="comment-count">${post.comments}</span>
@@ -329,15 +301,25 @@ document.addEventListener("DOMContentLoaded", async () => {
                     </a>
                     ${postActionsInsert}
                 `;
-                    postElement.appendChild(postActions);
+            postElement.appendChild(postActions);
 
-                    document.getElementById("posts").appendChild(postElement);
+            postElement.addEventListener("click", () => {
+                window.location.href = `/posts/${post._id}`;
+            });
+            return postElement;
+        }
 
-                    postElement.addEventListener("click", () => {
-                        window.location.href = `/posts/${post._id}`;
-                    });
-                }
-            }
+        if (userPosts) {
+            Promise.all(userPosts.posts.map((post) => loadPost(post)))
+                .then((elems) => {
+                    const postContainer = document.getElementById("posts");
+                    for (const e of elems) {
+                        postContainer.appendChild(e);
+                    }
+                })
+                .catch((e) => {
+                    console.error(e);
+                });
         }
     }
 });
